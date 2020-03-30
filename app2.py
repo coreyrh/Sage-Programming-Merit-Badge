5951a8b9e6956882865bb3628d1f0a4a5475e91f
Sage-Mayer <62776748+Sage-Mayer@users.noreply.github.com>
2020-03-30 16:16:17 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,32 +1,38 @@
-# What is "import" sys doing?
+# What is "import" sys doing? the import part is saying to import the sys. sys is what lets the interpreter understand
+# variables and other code a person would write
 import sys
+
 
 # This function takes input and validates that all of the required input is there
 def getArgs():
     try:
-        age, name = str(sys.argv[1], sys.argv[2])
+        age = str(sys.argv[1])
+        name = str(sys.argv[2])
     except:
-        print ('This program requires input.')
-        print ('Syntax: python app.py <age> <name>')
-        print ('Example: python app.py 40 Tom')
-        print ('')
-        print ('Using example as defaults')
-        print ('')
-        return ("40", "Tom")
+        print('This program requires input.')
+        print('Syntax: python app.py <age> <name>')
+        print('Example: python app2.py 40 Tom')
+        print('')
+        print('Using example as defaults')
+        print('')
+        return "40", "Tom"
 
     else:
-        return (age,name)
+        return age, name
+
 
 # Add an appropriate comment here
-def thePrintFunction( character_age, character_name):
-    print("there was a man named " + character_name + ", " )
-    print("he was " + character_age + "," )
-    print("he liked his name " + character_name + "," )
+def thePrintFunction(character_age, character_name):
+    print("there was a man named " + character_name + ", ")
+    print("he was " + character_age + ",")
+    print("he liked his name " + character_name + ",")
     print("but didn't like that he was " + character_age + ",")
+
 
 # Added bonus, create another function (not required)
 
-# what is this if statment doing?  
+# what is this if statement doing? This takes the info a person writes and assigns it to the variable base off the
+#order written in
 if __name__ == '__main__':
-   age, name = getArgs()
-   thePrintFunction(str(age), str(name))
+    age, name = getArgs()
+    thePrintFunction(str(age), str(name))
